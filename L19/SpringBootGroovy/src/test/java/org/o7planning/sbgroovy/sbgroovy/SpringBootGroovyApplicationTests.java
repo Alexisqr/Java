192325a73d24672e59408d88913f605fbdc838e4
Alexisqr <75916999+Alexisqr@users.noreply.github.com>
2023-10-06 01:45:19 +0300

    Add files via upload

---
+++ L19/SpringBootGroovy/src/test/java/org/o7planning/sbgroovy/sbgroovy/SpringBootGroovyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbgroovy.sbgroovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
